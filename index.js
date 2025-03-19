8e3395e37f1260e00c2b9cacb0a9349132719e61
Karan0207 <minidox0207@gmail.com>
2025-03-19 22:03:14 +0530

    add: uploads and folder fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// packages
+// Packages
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
+import cors from "cors";
 
-// Utiles
+// Utils
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -23,9 +23,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: 'https://frontend-marketplace-chi.vercel.app' // Allow requests from this origin
-}));
+app.use(
+  cors({
+    origin: "https://frontend-marketplace-chi.vercel.app", // Allow requests from this origin
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
@@ -38,6 +40,6 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "backend", "uploads")));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
